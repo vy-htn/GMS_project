e6cf635111c28affc53148612bd17d8e5ef7f9f1
@sangquangtryhard <sang7b7b@gmail.com>
2023-12-31 21:49:25 +0700

    update customer list

--- GMS_project/resources/views/customer_list.blade.php
+++ GMS_project/resources/views/customer_list.blade.php
@@ -5,32 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Spare parts list</title>
 </head>
-<link rel="stylesheet" href="../css/Spare-parts-list.css" />
+<link rel="stylesheet" href="../css/customer_list.css" />
 <body>
-    <!--
-    <body style="background-image: linear-gradient(to right, rgb(104, 222, 19), rgb(217, 255, 0)); 
-            background-repeat: no-repeat; 
-            background-attachment:fixed">-->
-            <div class="x_content">
+        <div class="x_content">
         <ul class="nav nav-tabs bar_tabs" role="tablist">
             <div class="container overflow-hidden">
                 <div class="row gx-5">
                     <div class="col">
                         <div class="p-3">
-                            <h2>Danh sách nhân viên</h2>
+                            <h2>Danh sách khách hàng</h2>
                         </div>
                     </div>
                     <div class="col">
                         <div class="p-3">
                             <form class="d-flex">
-                                <input class="form-control me-2" type="search" placeholder="Tìm nhân viên" aria-label="Search">
+                                <input class="form-control me-2" type="search" placeholder="Tìm khách hàng" aria-label="Search">
                                 <button class="btn btn-outline-success" type="submit">Tìm</button>
                             </form>
                         </div>
                     </div>
                     <div class="col">
                         <div class="p-3">
-                            <button type="button" class="btn btn-info">+ Thêm nhân viên</button>
+                            <button type="button" class="btn btn-info">+ Thêm khách hàng</button>
                         </div>
                     </div>
                 </div>
@@ -41,19 +37,19 @@
         <table>
             <tr>
                 <td>
-                    <h3>Danh sách phụ tùng</h3>
+                    <h3>Danh sách khách hàng</h3>
                 </td>
                 <td>
-                    <input type="text" style="width: 70%;" type="text" id="txtphutung" name="txtphutung" required />
+                    <input type="text" style="width: 70%;" type="text" id="txtCustomer" name="txtCustomer" required />
                 </td>
             </tr>
         
 </table>
     </div>
     <div>
-    <table cellpadding="3px" align="center" width="100%" class="spare-part-list">
+    <table cellpadding="3px" width="100%" class="customer_list">
         <tr class="title">
-            <th>Mã phụ tùng</th> <th>Tên phụ tùng</th> <th>Loại</th> <th>NCC</th> <th>Ngày nhập</th> <th>Giá</th> <th>SL</th> <th></th>
+            <th>Mã khách hàng</th> <th>Tên khách hàng</th> <th>Loại</th> <th>NCC</th> <th>Ngày nhập</th> <th>Giá</th> <th>SL</th> <th></th>
         </tr>
         <tr class="odd-line">
             <td>abc</td> <td>abc</td> <td>abc</td> <td>abc</td> <td>1/1/1</td> <td>123$</td> <td>12</td> <td>...</td>
